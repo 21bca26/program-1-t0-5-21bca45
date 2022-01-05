df763d8f984e3fee83009d27a4a7e8d03cb84839
21bca45 <96609099+21bca45@users.noreply.github.com>
2022-01-05 10:04:17 +0530

    Add files via upload

---
+++ Solutions/SOLUTION-2.C
@@ -0,0 +1,17 @@
+/* write a program to print digits in reverse order*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,i;
+	clrscr();
+	printf("\n Enter any Number:");
+	scanf("%d",&a);
+	while(a>0)
+	{
+		i=a%10;
+		printf("%d",i);
+		a=a/10;
+	}
+	getch();
+}
